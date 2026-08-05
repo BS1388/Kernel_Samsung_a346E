998881e59bd9a2fe9df6f65a3f703395ccdcc0e0
Babak <145232367+BS1388@users.noreply.github.com>
2026-08-06 02:50:42 +0330

    Update build_kernel.sh for kernel build configuration

--- build_kernel.sh
+++ build_kernel.sh
@@ -38,17 +38,17 @@ for f in $FTP; do
 done
 
 sed -i "s/-maybe-dirty//g" "build/kernel/kleaf/impl/stamp.bzl"
-sed -i "s/stable_scmversion_cmd = _get_status_at_path.*/stable_scmversion_cmd = \"echo \'\'\"/g" "build/kernel/kleaf/impl/stamp.bzl"
+sed -i "s/stable_scmversion_cmd = _get_status_at_path.*/stable_scmversion_cmd = \"echo ''\"/g" "build/kernel/kleaf/impl/stamp.bzl"
 sed -i 's|SOURCE_DATE_EPOCH=0|SOURCE_DATE_EPOCH=\\"$(date +%s)\\"|' "kernel_device_modules-6.6/scripts/gen_build_config.py"
 sed -i "s/r510928/r536225/" "kernel-6.6/build.config.constants"
 
-python kernel_device_modules-6.6/scripts/gen_build_config.py --kernel-defconfig mediatek-bazel_defconfig --kernel-defconfig-overlays "sec_ogki_fragment.config mt6877_overlay.config mt6877_teegris_5_overlay.config" --kernel-build-config-overlays "" -m user -o ../out/target/product/a34x/obj/KERNEL_OBJ/build.config
+python kernel_device_modules-6.6/scripts/gen_build_config.py --kernel-defconfig mediatek-bazel_defconfig --kernel-defconfig-overlays "mt6877_overlay.config mt6877_teegris_5_overlay.config" --kernel-build-config-overlays "" -m user -o ../out/target/product/a34x/obj/KERNEL_OBJ/build.config
 
 export DEVICE_MODULES_DIR="kernel_device_modules-6.6"
 export BUILD_CONFIG="../out/target/product/a34x/obj/KERNEL_OBJ/build.config"
 export OUT_DIR="../out/target/product/a34x/obj/KLEAF_OBJ"
 export DIST_DIR="../out/target/product/a34x/obj/KLEAF_OBJ/dist"
-export DEFCONFIG_OVERLAYS="sec_ogki_fragment.config mt6877_overlay.config mt6877_teegris_5_overlay.config"
+export DEFCONFIG_OVERLAYS="mt6877_overlay.config mt6877_teegris_5_overlay.config"
 export PROJECT="mgk_64_k66"
 export MODE="user"
 export SOURCE_DATE_EPOCH="$(date +%s)"
