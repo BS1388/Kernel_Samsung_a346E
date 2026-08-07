c6c8e309d644fcd63d8339edd2ce28bbe473383e
Babak <145232367+BS1388@users.noreply.github.com>
2026-08-07 08:05:25 +0330

    Add loop_device structure and necessary includes

--- kernel/kernel_device_modules-6.6/drivers/block/zram/zram_ext.c
+++ kernel/kernel_device_modules-6.6/drivers/block/zram/zram_ext.c
@@ -12,7 +12,41 @@
 #include <linux/freezer.h>
 #include <linux/highmem.h>
 #include <linux/kernel.h>
-#include <linux/loop.h>
+#include <linux/blk-mq.h>
+#include <linux/rbtree.h>
+#include <linux/timer.h>
+#include <linux/workqueue.h>
+
+struct loop_device {
+	int		lo_number;
+	loff_t		lo_offset;
+	loff_t		lo_sizelimit;
+	int		lo_flags;
+	char		lo_file_name[LO_NAME_SIZE];
+
+	struct file *	lo_backing_file;
+	struct block_device *lo_device;
+
+	gfp_t		old_gfp_mask;
+
+	spinlock_t		lo_lock;
+	int			lo_state;
+	spinlock_t              lo_work_lock;
+	struct workqueue_struct *workqueue;
+	struct work_struct      rootcg_work;
+	struct list_head        rootcg_cmd_list;
+	struct list_head        idle_worker_list;
+	struct rb_root          worker_tree;
+	struct timer_list       timer;
+	bool			use_dio;
+	bool			sysfs_inited;
+
+	struct request_queue	*lo_queue;
+	struct blk_mq_tag_set	tag_set;
+	struct gendisk		*lo_disk;
+	struct mutex		lo_mutex;
+	bool			idr_visible;
+};
 #include <linux/statfs.h>
 #include <linux/sec_mm.h>
 #include <linux/swap.h>
