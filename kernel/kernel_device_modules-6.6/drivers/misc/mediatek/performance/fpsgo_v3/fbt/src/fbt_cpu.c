d4e422ca52a0bf7b70c1e7dd83085fb86dd8ee89
Babak <145232367+BS1388@users.noreply.github.com>
2026-08-13 03:28:55 +0330

    Update fbt_cpu.c

--- kernel/kernel_device_modules-6.6/drivers/misc/mediatek/performance/fpsgo_v3/fbt/src/fbt_cpu.c
+++ kernel/kernel_device_modules-6.6/drivers/misc/mediatek/performance/fpsgo_v3/fbt/src/fbt_cpu.c
@@ -121,8 +121,13 @@
 #define FPSGO_BAFFINITY_TOTAL 7
 
 
+#include <linux/minmax.h>
+#ifndef MAX
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+#ifndef MIN
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#endif
 
 struct fbt_cpu_dvfs_info {
 	unsigned int *power;
